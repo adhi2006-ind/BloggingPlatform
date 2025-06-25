31de9eaf7e71b06f5128d34fff74800db3e41aa4
Adhithyan Antony <adhithyanantony44@gmail.com>
2025-06-25 19:51:55 +0530

    Delete blogging-platform/client directory

--- blogging-platform/client/src/pages/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-const NotFound = () => (
-  <div className="h-screen flex items-center justify-center">
-    <h1 className="text-3xl font-bold text-gray-600">404 – Page Not Found</h1>
-  </div>
-);
-
-export default NotFound;
